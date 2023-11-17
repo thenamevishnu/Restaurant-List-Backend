246a269ea928756862fd6f15f4015ad5b48ea540
thenamevishnu <mail.vishnumk@gmail.com>
2023-11-17 21:31:35 +0530

    Updates in response to frontend

--- Controller/RestaurantController.cjs
+++ Controller/RestaurantController.cjs
@@ -4,9 +4,8 @@ const addRestaurant = async (req, res) => {
     try {
         const object = req.body
         const response = await (await RestaurantList.create(object)).toJSON()
-        console.log(response);
         if (response.id) {
-            res.status(200).json({message: "Restaurant created"})
+            res.status(200).json({message: "Restaurant created", newRecord: response})
         } else {
             res.status(500).json({message: "Error while creating Restaurant"})
         }
@@ -18,7 +17,6 @@ const addRestaurant = async (req, res) => {
 const getRestaurant = async (req, res) => {
     try {
         const response = (await RestaurantList.findAll()).map(item => item.dataValues)
-        console.log(response);
         res.status(200).json({result: response})
     } catch (err) {
         res.status(500).json({message: err.message})
@@ -55,7 +53,8 @@ const updateRestaurant = async (req, res) => {
                 }
         })
         if (response[0]) {
-            res.status(200).json({message: "Updated"})
+            const list = await (await RestaurantList.findAll()).map(item => item.dataValues)
+            res.status(200).json({message: "Updated", newList: list})
         } else {
             res.status(404).json({message: "Record not found"})
         }
